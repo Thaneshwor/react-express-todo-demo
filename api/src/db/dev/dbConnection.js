a2537a904d915f4aea38d8653af608bb704a617e
Thaneshwor <thaneshwor100@gmail.com>
2020-04-19 11:17:42 +0545

    Add code to create users table

---
+++ api/src/db/dev/dbConnection.js
@@ -0,0 +1,30 @@
+import pool from './pool';
+
+pool.on('connect', () => {
+  console.log('Connected to db');
+});
+
+/**
+ * Create User Table
+ */
+
+const createUserTable = () => {
+  const userCreateQuery = `CREATE TABLE IF NOT EXISTS users
+    (id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    password VARCHAR(100) NOT NULL,
+    is_admin BOOL DEFAULT (false),
+    created_on DATE NOT NULL)`;
+
+  pool.query(userCreateQuery)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
