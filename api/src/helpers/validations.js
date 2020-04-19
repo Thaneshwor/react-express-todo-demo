86b91b1dbd4f49de548b21833889355d0dfd2f74
Thaneshwor <thaneshwor100@gmail.com>
2020-04-19 11:35:35 +0545

    Add function to validate password

--- api/src/helpers/validations.js
+++ api/src/helpers/validations.js
@@ -7,3 +7,14 @@ const isValidEmail = (email) => {
     const regEx = /\S+@\S+\.\S+/;
     return regEx.test(email);
 };
+
+/**
+   * validatePassword helper method
+   * @param {string} password
+   * @returns {Boolean} True or False
+   */
+const validatePassword = (password) => {
+    if (password.length <= 5 || password === '') {
+        return false;
+    } return true;
+};
