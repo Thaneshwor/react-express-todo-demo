44916de9ed666b350a3739f48702c797bc065ac6
Thaneshwor <thaneshwor100@gmail.com>
2020-04-19 12:48:18 +0545

    Add signinUser() function to sign in to app

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -5,6 +5,7 @@ import dbQuery from '../db/dev/dbQuery';
 
 import {
     hashPassword,
+    comparePassword,
     isValidEmail,
     validatePassword,
     isEmpty,
@@ -68,4 +69,48 @@ const createUser = async (req, res) => {
         errorMessage.error = 'Operation was not successful';
         return res.status(status.error).send(errorMessage);
     }
+};
+
+/**
+   * Signin
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} user object
+   */
+const signinUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (isEmpty(email) || isEmpty(password)) {
+        errorMessage.error = 'Email or Password detail is missing';
+        return res.status(status.bad).send(errorMessage);
+    }
+    if (!isValidEmail(email) || !validatePassword(password)) {
+        errorMessage.error = 'Please enter a valid Email or Password';
+        return res.status(status.bad).send(errorMessage);
+    }
+    const signinUserQuery = 'SELECT * FROM users WHERE email = $1';
+    try {
+        const { rows } = await dbQuery.query(signinUserQuery, [email]);
+        const dbResponse = rows[0];
+        if (!dbResponse) {
+            errorMessage.error = 'User with this email does not exist';
+            return res.status(status.notfound).send(errorMessage);
+        }
+        if (!comparePassword(dbResponse.password, password)) {
+            errorMessage.error = 'The password you provided is incorrect';
+            return res.status(status.bad).send(errorMessage);
+        }
+        const token = generateUserToken(dbResponse.email, dbResponse.id, dbResponse.is_admin, dbResponse.first_name, dbResponse.last_name);
+        delete dbResponse.password;
+        successMessage.data = dbResponse;
+        successMessage.data.token = token;
+        return res.status(status.success).send(successMessage);
+    } catch (error) {
+        errorMessage.error = 'Operation was not successful';
+        return res.status(status.error).send(errorMessage);
+    }
+};
+
+export {
+    createUser,
+    signinUser,
 };
