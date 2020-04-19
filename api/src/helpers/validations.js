2660d57f083c8549ff8162525f6d855c77eda218
Thaneshwor <thaneshwor100@gmail.com>
2020-04-19 11:34:46 +0545

    Add function to validate email

---
+++ api/src/helpers/validations.js
@@ -0,0 +1,9 @@
+/**
+   * isValidEmail helper method
+   * @param {string} email
+   * @returns {Boolean} True or False
+   */
+const isValidEmail = (email) => {
+    const regEx = /\S+@\S+\.\S+/;
+    return regEx.test(email);
+};
